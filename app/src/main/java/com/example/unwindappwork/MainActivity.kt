f3b899eaa4e1cd3d77ca5be57f0350b6971a7ea2
Sparsh <ssparsh100@gmail.com>
2024-12-31 19:03:25 +0530

    Track Order, Refer a friend, demo videos UI implemented

---
+++ app/src/main/java/com/example/unwindappwork/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.unwindappwork
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.track_order)
+
+    }
+}
